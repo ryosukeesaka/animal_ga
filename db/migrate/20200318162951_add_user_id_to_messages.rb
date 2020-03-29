6336505319e133122ffff97cbcbbc639f923b3c8
ryosukeesaka <ryokexexex4515@gmail.com>
2020-03-29 06:04:54 +0000

    [fixed]カラム修正

--- db/migrate/20200318162951_add_user_id_to_messages.rb
+++ db/migrate/20200318162951_add_user_id_to_messages.rb
@@ -1,7 +1,6 @@
 class AddUserIdToMessages < ActiveRecord::Migration[5.2]
   def change
-    add_column :messages, :user_id, :integer
-    add_column :messages, :user, :refarences, foreign_key: true
-    add_column :messages, :room, :refarences, foreign_key: true
+    add_column :messages, :user,null: false, foreign_key: true
+    add_column :messages, :room,null: false,  foreign_key: true
   end
 end
